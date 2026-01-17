3102385916daa8c65ec03eebe206ba44449ee953
Rahul Bera <rahulbera3654@gmail.com>
2026-01-17 13:33:41 +0530

    Change in the url of frontend

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://chat-withpdf-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
