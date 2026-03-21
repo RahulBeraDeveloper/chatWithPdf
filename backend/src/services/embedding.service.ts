2ef25ef66f46a9bd00d32091fe4d55273c8ad18b
Rahul Bera <rahulbera3654@gmail.com>
2026-03-21 18:01:03 +0530

    Change done again embedding file

--- backend/src/services/embedding.service.ts
+++ backend/src/services/embedding.service.ts
@@ -1,29 +1,4 @@
 
-// import { pipeline } from "@xenova/transformers";
-
-// class EmbeddingService {
-//   private extractor: any;
-
-//   async init() {
-//     if (!this.extractor) {
-//       this.extractor = await pipeline(
-//         "feature-extraction",
-//         "Xenova/all-MiniLM-L6-v2"
-//       );
-//     }
-//   }
-
-//   async embedText(text: string): Promise<number[]> {
-//     await this.init();
-//     const out = await this.extractor(text, {
-//       pooling: "mean",
-//       normalize: true,
-//     });
-//     return Array.from(out.data);
-//   }
-// }
-
-// export default new EmbeddingService();
 import { pipeline } from "@xenova/transformers";
 
 class EmbeddingService {
@@ -40,13 +15,11 @@ class EmbeddingService {
 
   async embedText(text: string): Promise<number[]> {
     await this.init();
-
-    const output = await this.extractor(text, {
+    const out = await this.extractor(text, {
       pooling: "mean",
       normalize: true,
     });
-
-    return Array.from(output.data);
+    return Array.from(out.data);
   }
 }
 
